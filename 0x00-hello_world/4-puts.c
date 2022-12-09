3ba330e6d5cbe613c49bfd506cb542e21bfb8feb
fastbike22 <fred2doma@gmail.com>
2022-12-08 16:18:40 -0800

    Ex4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
-	return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
 }
